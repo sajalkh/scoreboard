afbc96a70580a6027c9707b63e0c423a273c8533
Sajal Khanal <sajalkhanal01@gmail.com>
2026-05-20 18:10:55 -0600

    commit initial files

---
+++ index.js
@@ -0,0 +1,31 @@
+count1 = 0
+count2 = 0
+function pOneh(){
+    count1 += 1
+    document.getElementById("h").textContent = count1 
+}
+
+function pOneg(){
+    count2 += 1
+    document.getElementById("g").textContent = count2  
+}
+
+function pTwoh(){
+    count1 += 2
+    document.getElementById("h").textContent = count1 
+}
+
+function pTwog(){
+    count2 += 2
+    document.getElementById("g").textContent = count2  
+}
+
+function pThreeh(){
+    count1 += 3
+    document.getElementById("h").textContent = count1 
+}
+
+function pThreeg(){
+    count2 += 3
+    document.getElementById("g").textContent = count2  
+}
